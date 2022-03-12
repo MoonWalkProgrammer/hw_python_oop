73d985e8da1324876a08fb53c41d2654e60ef40f
Александр Водяницкий <alexvod550@yandex.ru>
2022-03-13 01:10:00 +0300

    Second Try

--- homework.py
+++ homework.py
@@ -11,10 +11,10 @@ class InfoMessage:
     speed: float
     calories: float
     MESSAGE: str = ('Тип тренировки: {training_type}; '
-               'Длительность: {duration:.3f} ч.; '
-               'Дистанция: {distance:.3f} км; '
-               'Ср. скорость: {speed:.3f} км/ч; '
-               'Потрачено ккал: {calories:.3f}.')
+                    'Длительность: {duration:.3f} ч.; '
+                    'Дистанция: {distance:.3f} км; '
+                    'Ср. скорость: {speed:.3f} км/ч; '
+                    'Потрачено ккал: {calories:.3f}.')
 
     def get_message(self) -> str:
         '''Return information about a completed excercise.'''
